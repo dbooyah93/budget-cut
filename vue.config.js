a0560dbab8992a0bdedda07205ca9b7751f4d395
Matthew Boyea <yourzombiemop@gmail.com>
2022-07-13 13:12:32 -0500

    initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: 'bin',
+  pages: {
+    index: {
+      entry: 'src/index.ts',
+      template: 'src/index.html',
+      title: 'TS Vue Template',
+    },
+  },
 });
