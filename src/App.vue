4f7dc443c8be5d82ae27c3b85906307c34450bbe
dbooyah93 <davidmcruz1125@gmail.com>
2022-07-26 17:16:26 -0500

    Implements some beginner ts syntax, data() is incorrect to use in ts file

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,50 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'App',
+    name: 'App',
+    data () {
+        return {
+            msg: 'Welcome to your Vue.js App',
+            year: new Date().getFullYear(),
+            months: [
+                'January',
+                'Feburary',
+                'Martch',
+                'April',
+                'May',
+                'June',
+                'July',
+                'August', 
+                'September',
+                'October',
+                'November',
+                'December'
+            ],
+            month: '',
+            listItems: [],
+        }
+    },
+    methods: {
+        postListItem: function ( listItem:String ) {
+            let request = new XMLHttpRequest();
+            let date = new Date().getDay();
+            let month = new Date().getMonth();
+            let year = new Date().getFullYear();
+            if ( listItem !== undefined ) {
+                date = Number(listItem.split('/')[0]);
+                month = Number(listItem.split('/')[1]);
+                year = Number(listItem.split('/')[2]);
+            }
+            request.addEventListener('load', (event:Event) => {
+                this.getListItems();
+            });
+
+        },
+        getListItems: function () {
+            let month = this.month;
+            let year = this.year;
+        }
+    }
 });
 </script>
 
