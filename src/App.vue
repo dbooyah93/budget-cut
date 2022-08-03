95c8ccafc56ae5a956a49de4af3bfecaf4471ffd
dbooyah93 <davidmcruz1125@gmail.com>
2022-08-02 22:04:15 -0500

    Some additions

--- src/App.vue
+++ src/App.vue
@@ -41,11 +41,30 @@ export default defineComponent({
                 year = Number(listItem.split('/')[2]);
             }
             request.addEventListener('load', (event:Event) => {
-                this.getListItems();
+                if ( event ) {
+                    console.log('there was an err');
+                    console.trace( event );
+                } else {
+                    this.getListItems(); // changed variable name for clarity
+                    console.log('ok');
+                }
             });
+            request.addEventListener('error', (err:Event) => {
+                console.log({err});
+            });
+            request.open('POST', 'http://192.168.86.23:3000/' + date + '/' + month + '/' + year);
+            request.send();
 
         },
         getListItems: function () {
+            let request = new XMLHttpRequest();
+            request.addEventListener('load', (res, err) => {
+                if ( err ) {
+                    console.log("there was an error");
+                } else {
+                    this.listItems = JSON.parse(res.target.response).items;
+                }
+            })
             let month = this.month;
             let year = this.year;
         }
